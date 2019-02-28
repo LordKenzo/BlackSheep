03de8453faa0ebe6d298f8dc3d35765e8c3ddd22
LordKenzo <lordkenzo@outlook.it>
2019-02-28 18:21:09 +0100

    Fix Header - Add Passive Events Script as CDN resource

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,10 @@
 import { Component } from '@angular/core';
-import { EasingLogic } from 'ngx-page-scroll-core';
 
 @Component({
   selector: 'bs-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'BlackSheep';
-  public myEasing: EasingLogic = (t: number, b: number, c: number, d: number): number => {
-    // easeInOutExpo easing
-    if (t === 0) {
-      return b;
-    }
-    if (t === d) {
-      return b + c;
-    }
-    if ((t /= d / 2) < 1) {
-      return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
-    }
-
-    return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
-  }
-
-  doSmth(reachedTarget: boolean): void {
-      if (reachedTarget) {
-          console.log('Yeah, we reached our destination');
-      } else {
-          console.log('Ohoh, something interrupted us');
-      }
-  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { EasingLogic } from 'ngx-page-scroll-core';
 
 @Component({
   selector: 'bs-header',
-  templateUrl: 'header.component.html'
+  templateUrl: 'header.component.html',
 })
 export class HeaderComponent {
+  public myEasing: EasingLogic = (t: number, b: number, c: number, d: number): number => {
+    // easeInOutExpo easing
+    if (t === 0) {
+      return b;
+    }
+    if (t === d) {
+      return b + c;
+    }
+    if ((t /= d / 2) < 1) {
+      return (c / 2) * Math.pow(2, 10 * (t - 1)) + b;
+    }
 
+    return (c / 2) * (-Math.pow(2, -10 * --t) + 2) + b;
+  }
+
+  doSmth(reachedTarget: boolean): void {
+    if (reachedTarget) {
+      console.log('Yeah, we reached our destination');
+    } else {
+      console.log('Ohoh, something interrupted us');
+    }
+  }
 }
