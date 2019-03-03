1fcafe7059747011e982695d169f8a1b7f7b57e5
LordKenzo <lordkenzo@outlook.it>
2019-03-03 18:38:10 +0100

    Some fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
-import { fireKey } from './firebase.key';
-
 export const environment = {
-  firebase: fireKey,
+  firebase: {
+    apiKey: 'AIzaSyByjKKcdlicP2zAR4Bb0qhQEy07BiKw8-U',
+    authDomain: 'theblacksheepgym.firebaseapp.com',
+    databaseURL: 'https://theblacksheepgym.firebaseio.com',
+    projectId: 'theblacksheepgym',
+    storageBucket: 'theblacksheepgym.appspot.com',
+    messagingSenderId: '320383800595',
+  },
   production: true,
 };
