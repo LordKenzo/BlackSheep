f629b73a3d08286edff97c58e5559cc45d5b1794
LordKenzo <lordkenzo@outlook.it>
2019-03-03 11:13:35 +0100

    A Login Test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  }
+];
 /*const routerOptions: ExtraOptions = {
   useHash: false,
   anchorScrolling: 'enabled',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,40 @@
+// Moduli Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { environment } from '../environments/environment';
 
+// Quando creo lo sharedModule sposta questi:
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+// Componenti Terze Parti
 import { NgxPageScrollModule } from 'ngx-page-scroll';
+
+// Firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
+// Miei Componenti
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 
+// Variabili di Ambienti
+import { environment } from '../environments/environment';
+import { LoginComponent } from './components/login/login.component';
+
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, BannerComponent],
+  declarations: [AppComponent, HeaderComponent, BannerComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgxPageScrollModule,
     HttpClientModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [{ provide: FirestoreSettingsToken, useValue: {} }],
   bootstrap: [AppComponent],
