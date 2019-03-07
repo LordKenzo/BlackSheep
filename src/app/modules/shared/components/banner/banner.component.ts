4bfe0bb197dbb24c9b03a5a8559e92f2ad868104
LordKenzo <>
2019-03-07 15:11:59 +0100

    Working on Responsive Image on Banner

--- src/app/modules/shared/components/banner/banner.component.ts
+++ src/app/modules/shared/components/banner/banner.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BannerService } from './banner.service';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'bs-banner',
@@ -10,9 +12,45 @@ export class BannerComponent implements OnInit {
   subtitle = 'Forge Body';
   description = 'Premium Fitness in Ladispoli';
   images;
-  constructor() {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private bannerService: BannerService) {}
 
   ngOnInit() {
-
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge
+    ]).subscribe(result => {
+      if (result.breakpoints[Breakpoints.XSmall]) {
+        console.log('XSmall');
+      }
+      if (result.breakpoints[Breakpoints.Small]) {
+        console.log('Small');
+      }
+      if (result.breakpoints[Breakpoints.Medium]) {
+        console.log('Medium');
+      }
+      if (result.breakpoints[Breakpoints.Large]) {
+        console.log('Large');
+      }
+      if (result.breakpoints[Breakpoints.XLarge]) {
+        console.log('XLarge');
+      }
+    });
+    this.bannerService.getBanner().subscribe((data: any) => {
+      this.images = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          fileName: e.payload.doc.data()['fileName'],
+          fileNameHi: e.payload.doc.data()['fileNameHi'],
+          resolution: e.payload.doc.data()['resolution'],
+          resolutionHi: e.payload.doc.data()['resolutionHi'],
+          media: e.payload.doc.data()['media'],
+        };
+      });
+    });
   }
 }
--- src/app/modules/shared/components/banner/banner.service.ts
+++ src/app/modules/shared/components/banner/banner.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { ResolutionService } from 'src/app/modules/core/services/resolution.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BannerService {
 
-  constructor(private firestore: AngularFirestore) {}
+  constructor(
+    private resolutionService: ResolutionService,
+    private firestore: AngularFirestore) {}
 
   getBanner() {
+    console.log(this.resolutionService.getIsMobileResolution());
     return this.firestore.collection('Banner').snapshotChanges();
   }
 }
